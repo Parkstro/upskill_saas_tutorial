17d497778496d46b4058dd539ebebee470abe649
Parker Strom <stromparker@comcast.net>
2017-01-05 22:35:49 +0000

    Added code comments and attempted to fix problems with plans

--- app/controllers/users/registrations_controller.rb
+++ app/controllers/users/registrations_controller.rb
@@ -1,4 +1,7 @@
 class Users::RegistrationsController < Devise::RegistrationsController
+  # Extend default devise gem behaivor so that users signing up with Pro
+  # savewith a special Stripe subscription function.
+  # Otherwise Devise devise signs user up as usual 
   def create
     super do |resource|
       if params[:plan]
