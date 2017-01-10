0410f6d13e10d9937501feb4fc402ae17f6dc63a
Parker Strom <stromparker@comcast.net>
2017-01-10 17:12:55 +0000

    Added user profiles form

--- app/controllers/profiles_controller.rb
+++ app/controllers/profiles_controller.rb
@@ -1,1 +1,9 @@
 class ProfilesController < ApplicationController
+   
+   # Get to /users/:user_id/profile/new
+   def new
+      # Render blank profile details form 
+      @profile = Profile.new
+   end
+   
+end
