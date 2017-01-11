940d00fbb9f016158ec664f336f6feb0f9f96447
Parker Strom <stromparker@comcast.net>
2017-01-11 20:55:59 +0000

    Added profiles create action and tried a new debugging gem

--- app/controllers/profiles_controller.rb
+++ app/controllers/profiles_controller.rb
@@ -6,4 +6,22 @@ class ProfilesController < ApplicationController
       @profile = Profile.new
    end
    
+   # POST to /users/:user_id/profile
+   
+   def create
+      @user = User.find( params[:user_id] )
+      @profile = @user.build_profile( profile_params )
+      if @profile.save
+         flash[:success] = "Profile Updated!"
+         redirect_to root_path
+      else
+         render action: :new
+      end
+   end
+   
+   private
+      def profile_params
+         params.require(:profile).permit(:first_name, :last_name, :job_title, :contact_email, :desctiption)
+      end
+   
 end
